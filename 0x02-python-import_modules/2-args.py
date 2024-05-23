0cfb3273451bb122c4a61cb36d2f918644d4a05b
1duuude8 <younesarab3@gmail.com>
2024-05-23 16:31:07 +0200

    code for task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+import sys
+
+if __name__ == "__main__":
+    num_arg = len(sys.argv) - 1
+
+    if num_arg == 0:
+        print("0 arguments.")
+    else:
+        print("{} argument{}:".format(num_arg, "" if num_arg == 1 else "s"))
+        for i, arg in enumerate(sys.argv[1:], start=1):
+            print("{} : {}".format(i, arg))
