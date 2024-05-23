f1501af2838914839a98926b772b97245d3d06af
1duuude8 <younesarab3@gmail.com>
2024-05-23 16:35:54 +0200

    code for task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     else:
         print("{} argument{}:".format(num_arg, "" if num_arg == 1 else "s"))
         for i, arg in enumerate(sys.argv[1:], start=1):
-            print("{} : {}".format(i, arg))
+            print("{}: {}".format(i, arg))
