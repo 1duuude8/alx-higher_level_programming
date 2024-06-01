dc3ecba69525b7a06209acb7ecd80f0bea89edf6
1duuude8 <younesarab3@gmail.com>
2024-06-01 11:12:30 +0200

    code for task 4

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    """
+    Return a set of elements that are in either of the sets but not in both.
+
+    args:
+        set_1 (set): the first set
+        set_2 (set): the second set
+
+    returns:
+        set: A set containing elements that are in either of the sets but not in both.
+    """
+    return set_1 ^ set_2
