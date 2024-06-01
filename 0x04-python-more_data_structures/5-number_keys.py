11beff1f898106d4abaa5c8edc5fa510f1676241
1duuude8 <younesarab3@gmail.com>
2024-06-01 11:45:50 +0200

    code for task 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """
+    counts number of keys in a dict
+
+    args:
+        a_dictionary (dict): a dictonarty containing elements
+    returns:
+        int: return number of keys in a dictionnary
+    """
+    return len(a_dictionary)
