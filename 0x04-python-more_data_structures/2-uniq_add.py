f7e5f6874a8f1d53dd93dd750548a089fe5f65ba
1duuude8 <younesarab3@gmail.com>
2024-06-01 10:57:49 +0200

    code for task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    """
+    calculate the sum of unique element of a list
+
+    args :
+        my_list (list) : list of integer
+
+    return :
+        int : the sum of unique element in a list
+    """
     return sum(set(my_list))
