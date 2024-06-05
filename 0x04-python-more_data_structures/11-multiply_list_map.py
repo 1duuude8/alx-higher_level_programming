7d69e82f9a9eacfab2e3cb553a4b77462a7d8bce
1duuude8 <younesarab3@gmail.com>
2024-06-05 09:11:06 +0200

    code for task 11

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -10,4 +10,11 @@ def multiply_list_map(my_list=[], number=0):
     return
         list: A new list containing the multiplied elements
     """
-    return list(map(lambda n: n * number, my_list))
+    new_list = list(map(lambda n: n * number, my_list))
+    return new_list
+
+
+my_list = [1, 2, 3, 4, 6]
+new_list = multiply_list_map(my_list, 4)
+print(new_list)
+print(my_list)
